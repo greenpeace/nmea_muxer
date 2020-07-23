fc9b59492d67f98e319adb79c7fc77b90f3e61c9
yakup <yakup.cetinkaya@gmail.com>
2020-07-23 15:20:30 +0200

    logging

--- lib/utils.py
+++ lib/utils.py
@@ -60,7 +60,7 @@ def pprint(msg,sender="SYSTEM",severity="INFO"):
         "ERROR"     : Fore.RED
         }
 
-    log = Fore.MAGENTA+Style.DIM + dt.now().strftime("%y%m%d%H%M%S")+" "+sev_times.get(severity) + severity.rjust(5)+ " "+Style.RESET_ALL+" "+sen_colors.get(sender)+sender.ljust(8," ")+" "+sev_colors.get(severity)+msg+Style.RESET_ALL
+    log = Fore.MAGENTA+Style.DIM + dt.now().strftime("%y%m%d%H%M%S")+" "+sev_times.get(severity) + severity[0]+ " "+Style.RESET_ALL+" "+sen_colors.get(sender)+sender.ljust(8," ")+" "+sev_colors.get(severity)+msg+Style.RESET_ALL
     if severity == "DEBUG":
         logging.debug(log)
     elif severity == "INFO":
